e84ca48c49212403e2f5ad1674aef99917f820a1
Justin Ellis <jdellis490@gmail.com>
2022-04-10 19:41:39 -0700

    If/else statement attempts

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
+//Header live time
 var now = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
 var headerEL = document.querySelector('#currentDay');
 headerEL.textContent = now
 
-var inputTask = document.createElement("input");
-inputTask.setAttribute("type", "text");
-inputTask.textContent = "";
+var hourNow = moment().hour();
+console.log(hourNow);
 
-// $(document).ready(function(){
-//     if 
-// })
+//Might have to for loop instead for each time
+console.log($("#hour-9").attr("data-hour"))
+
+if (hourNow == $("#hour-9").attr("data-hour")){
+    $("#hour-9").siblings("textarea").addClass("past");
+} else if (hourNow < $("#hour-9").attr("data-hour")){
+    $("textarea").addClass("future");
+} else if (hourNow > $("#hour-9").attr("data-hour")){
+    $("textarea").addClass("past");
+}
 
 
-var saveButton = ('saveBtn');
-(saveButton).on('click', function(){
-    var hourTask = inputTask.value;
-    localStorage.setItem("Hour-9", hourTask);
-})
-
-
+$('.saveBtn').on('click', function(){
+    localStorage.setItem("div.hour").attr("data-hour"), $("text-area").val()
+});
