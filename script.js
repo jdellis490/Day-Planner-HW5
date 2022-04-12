7d2958de5e57c6ee6bc032407e6218c4ba574b2b
Justin Ellis <jdellis490@gmail.com>
2022-04-11 22:47:33 -0700

    Cleaning up code

--- script.js
+++ script.js
@@ -1,23 +1,29 @@
-//Header live time
+
+// Header live time
 var now = moment().format('dddd, MMMM Do YYYY, h:mm:ss a');
 var headerEL = document.querySelector('#currentDay');
 headerEL.textContent = now
 
+// Current hour for time of day usage for calendar
 var hourNow = moment().hour();
 console.log(hourNow);
-
-//Might have to loop instead for each time
 console.log($("#hour-9").attr("data-hour"))
 
-if (hourNow == $("#hour-9").attr("data-hour")){
-    $("#hour-9").siblings("textarea").addClass("present");
-} else if (hourNow < $("#hour-9").attr("data-hour")){
-    $("textarea").addClass("future");
-} else if (hourNow > $("#hour-9").attr("data-hour")){
-    $("textarea").addClass("past");
+// Might have to loop to change the background of each text area
+
+for (i = 9; i < 18; i++) {
+    if (i == hourNow){
+        $('textarea').eq(i - 9).addClass('present')
+    }else if (i < hourNow){
+        $('textarea').eq(i - 9).addClass('past')
+    }else { 
+        $('textarea').eq(i - 9).addClass('future')
+    }
 }
 
-
+//Save button to get text area input to local storage
 $('.saveBtn').on('click', function(){
-    localStorage.setItem($(this).siblings("div.hour").attr("data-hour"), $(this).siblings("text-area").val())
-});
+       localStorage.setItem($(this).siblings("div.hour").attr("data-hour"), $(this).siblings("textarea").val())
+   });
+
+   console.log($('.').siblings('div.hour').attr('data-hour'));
